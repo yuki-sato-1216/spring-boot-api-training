5eb6fe1e947c60a04201805a4fca5e2c6239ea4a
Yuki Sato <devyusa1216@gmail.com>
2024-12-17 17:42:30 +0900

    初期データ修正

--- api-demo/src/main/resources/data.sql
+++ api-demo/src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (name, email, is_active, created_at, updated_at)
-VALUES ('User', 'test@example.com', TRUE, LOCALTIME(), LOCALTIME());
+VALUES ('User1', 'test@example.com', TRUE, LOCALTIME(), LOCALTIME());
